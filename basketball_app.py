52d8fa3513ed04f10d42d7dfee36704036a69463
Michal Tesler <michal.tesler@gmail.com>
2022-09-01 07:27:52 +0200

    Add application header with short description

---
+++ basketball_app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import base64
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+st.title('NBA Player Stats Explorer')
+
+st.markdown("""
+This app performs simple webscraping of NBA player stats data.
+""")
