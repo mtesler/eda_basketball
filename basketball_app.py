54a185d3b154a4283b1ebf1229d3361fd24d7595
Michal Tesler <michal.tesler@gmail.com>
2022-09-02 06:30:20 +0200

    Add sidebar with select the year option

--- basketball_app.py
+++ basketball_app.py
@@ -10,3 +10,6 @@ st.title('NBA Player Stats Explorer')
 st.markdown("""
 This app performs simple webscraping of NBA player stats data.
 """)
+
+st.sidebar.header('User Input Features')
+selected_year = st.sidebar.selectbox('Year', list(reversed(range(1950, 2022))))
