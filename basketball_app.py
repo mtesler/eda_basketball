47a213cda3d4da26fc3e9582a748214552e40c94
Michal Tesler <michal.tesler@gmail.com>
2022-09-05 18:34:13 +0200

    Download NBA Player stats into a table

--- basketball_app.py
+++ basketball_app.py
@@ -13,3 +13,21 @@ This app performs simple webscraping of NBA player stats data.
 
 st.sidebar.header('User Input Features')
 selected_year = st.sidebar.selectbox('Year', list(reversed(range(1950, 2022))))
+
+# Web scraping of NBA player stats
+
+
+@st.cache
+def load_data(year):
+    url = "https://www.basketball-reference.com/leagues/NBA_2021_totals.html"
+    html = pd.read_html(url, header=0)
+    df = html[0]
+    raw = df.drop(df[df.Age == 'Age'].index)
+    raw = df
+    raw = raw.fillna(0)
+    player_stats = raw.drop(['Rk'], axis=1)
+    return player_stats
+
+
+player_stats = load_data(selected_year).astype(str)
+st.write(player_stats)
