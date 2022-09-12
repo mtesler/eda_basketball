f551802804ccd1feb573a8a4b6ae39f1ef46268d
Michal Tesler <michal.tesler@gmail.com>
2022-09-12 07:20:14 +0200

    Create heatmap with player stats data

--- basketball_app.py
+++ basketball_app.py
@@ -66,5 +66,14 @@ def file_download(df):
 st.markdown(file_download(df_selected_options), unsafe_allow_html=True)
 
 # Heatmap
+st.set_option('deprecation.showPyplotGlobalUse', False)
 df_selected_options.to_csv('output.csv', index=False)
 df = pd.read_csv('output.csv')
+
+corr = df.corr()
+mask = np.zeros_like(corr)
+mask[np.triu_indices_from(mask)] = True
+with sns.axes_style("white"):
+    f, ax = plt.subplots(figsize=(7, 5))
+    ax = sns.heatmap(corr, mask=mask, vmax=1, square=True)
+st.pyplot()
